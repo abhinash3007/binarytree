978adfb026fd6f8909727f78499c1353b1d698fd
Abhinash Mishra <abhimishrabhi2580@gmail.com>
2024-05-10 23:20:41 +0530

    binaryTreesDay4

---
+++ LevelOderTraversal.java
@@ -0,0 +1,48 @@
+package BINARYTREE;
+
+import java.util.*;
+
+public class LevelOderTraversal {
+    public static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    public static void level(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int level = q.size();
+            for (int i = 0; i < level; i++) {
+                Node current = q.remove();
+                System.out.print(current.data);
+                if (current.left != null) {
+                    q.add(current.left);
+                }
+                if (current.right != null) {
+                    q.add(current.right);
+                } 
+            }System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        node.left = new Node(2);
+        node.right = new Node(3);
+        node.left.left = new Node(4);
+        node.left.right = new Node(5);
+        node.right.right = new Node(6);
+        level(node);
+    }
+}
